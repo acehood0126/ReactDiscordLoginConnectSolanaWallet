816da35320a4d4a96b60b60a5156ebd26bfcd2bd
Ace Hood <101823587+acehood0126@users.noreply.github.com>
2022-05-30 20:27:11 +0900

    50%

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,27 +1,11 @@
 var express = require("express");
 var router = express.Router();
 const DiscordOauth2 = require("discord-oauth2");
+const { json } = require("express/lib/response");
 
-/* GET home page. */
-router.get("/", function (req, res, next) {
-  res.render("index", { title: "Express" });
-});
-
-router.get("/discordauthenticate", function (req, res, next) {
-  const oauth = new DiscordOauth2();
-
-  oauth
-    .tokenRequest({
-      clientId: "934363403550593024",
-      clientSecret: "WSVa1qcqz1Ger-Km9RebOgfjIgk5VBvK",
-
-      code: "query code",
-      scope: "identify guilds",
-      grantType: "authorization_code",
-
-      redirectUri: "http://localhost:3000/login/discord",
-    })
-    .then(console.log);
+router.post("/addnewuser", function (req, res, next) {
+  const userdata = req.body;
+  res.send(userdata);
 });
 
 module.exports = router;
